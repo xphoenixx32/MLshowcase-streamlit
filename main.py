cf8f92fbe10ed7c395c04a6fafe2e4198e5faa4e
Lean Lin <xphoenixx32@gmail.com>
2025-01-05 17:39:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -24,6 +24,8 @@ from streamlit_option_menu import option_menu
 
 st.title("a Demo of ML Model & XAI")
 st.caption("*using **Machine Learning** method to modeling seaborn dataset, leveraging **SHAP value** to explain the explanatory power of independent variables*")
+st.logo("balloon")
+
 sns.set_theme(style = "whitegrid")
 #------------------------------------------------------------------------------------------------------#
 
