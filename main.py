8225d0a618a8082aa6ad1bc098d488be9419eaa1
Lean Lin <xphoenixx32@gmail.com>
2025-01-09 12:21:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ from lightgbm import LGBMRegressor
 from streamlit_option_menu import option_menu
 #------------------------------------------------------------------------------------------------------#
 
-st.title(" 🎛️ Demo of *Machine Learning Model* & *Explainable AI* ")
+st.header(" 🎛️ Demo of *Machine Learning Model* & *Explainable AI* ")
 st.caption('''
 *this app tries to standerdize a process of understanding a **Machine Learning Model** Performance with meaningful metrics & visualizations*
 ''')
